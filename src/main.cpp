4a1ac6eb120b962c3160aa97532b1fb6de25152d
denghe <denghe@hotmail.com>
2023-08-14 18:57:41 +0800

    writing...

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include "pch.h"
 
 template<typename T> concept Has_Init = requires(T t) { t.Init(); };
-template<typename T> concept Has_Init_ = requires(T t) { t.Init_(); };
 template<typename T> concept Has_Update = requires(T t) { t.Update(); };
 template<typename T> concept Has_Draw = requires(T t) { t.Draw(); };
 
 template<typename Derived>
 struct Engine {
-    float fps = 60, frameDelay = 1.f / fps, maxFrameDelay = frameDelay * 3;
-    float w = 800, h = 600;
+    static constexpr float fps = 60, frameDelay = 1.f / fps, maxFrameDelay = frameDelay * 3;
+    static constexpr float w = 1280, h = 720;
 
-    double dpr = emscripten_get_device_pixel_ratio();
-    double nowSecs{}, delta{};                        // fill by js callback
+    EMSCRIPTEN_WEBGL_CONTEXT_HANDLE glContext;
+    double devicePixelRatio{};
 
+    double nowSecs{}, delta{};
     double timePool{};
     int frameNumber{};
     EM_BOOL running{ EM_TRUE };
@@ -37,13 +37,53 @@ struct Engine {
         return running;
     }
 
+    template<GLenum shaderType>
+    inline static GLuint CompileShader(const char *src) {
+        GLuint shader = glCreateShader(shaderType);
+        glShaderSource(shader, 1, &src, nullptr);
+        glCompileShader(shader);
+        return shader;
+    }
+
+    inline static GLuint create_program(GLuint vertexShader, GLuint fragmentShader) {
+        GLuint program = glCreateProgram();
+        glAttachShader(program, vertexShader);
+        glAttachShader(program, fragmentShader);
+        glBindAttribLocation(program, 0, "pos");
+        glLinkProgram(program);
+        glUseProgram(program);
+        return program;
+    }
+
+    static GLuint create_texture() {
+        GLuint texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        return texture;
+    }
+
+
     Engine() {
+        // todo: init js, gl
+
+        double dpr = emscripten_get_device_pixel_ratio();
+        emscripten_set_element_css_size("canvas", w / dpr, h / dpr);
+        emscripten_set_canvas_element_size("canvas", w, h);
+
+        EmscriptenWebGLContextAttributes attrs;
+        emscripten_webgl_init_context_attributes(&attrs);
+        attrs.alpha = 0;
+        attrs.majorVersion = 2;
+        glContext = emscripten_webgl_create_context("canvas", &attrs);
+        xx_assert(glContext);
+        emscripten_webgl_make_context_current(glContext);
+
         if constexpr(Has_Init<Derived>) {
             ((Derived*)this)->Init();
-        }
-        // todo: init js, gl
-        if constexpr(Has_Init_<Derived>) {
-            ((Derived*)this)->Init_();
         }
     }
 };
@@ -51,17 +91,18 @@ struct Engine {
 
 struct GameLooper : Engine<GameLooper> {
     void Init() {
-        w = 1280;
-        h = 720;
-        tasks.Add(MainLogic());
+        tasks.Add([this]()->xx::Task<> {
+            // todo
+            for (int i = 0; i < 120; ++i) {
+                std::cout << nowSecs << " : " << frameNumber << std::endl;
+                co_yield 0;
+            }
+            running = false;
+        });
     }
-    xx::Task<> MainLogic() {
-        // todo: logic here
-        for (int i = 0; i < 120; ++i) {
-            std::cout << nowSecs << " : " << frameNumber << std::endl;
-            co_yield 0;
-        }
-        running = false;
+
+    void Draw() {
+        // todo
     }
 };
 
